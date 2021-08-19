2a9eed1f66d176bd654c598c3c1c0bace164ebe3
simpsone4652 <61930594+simpsone4652@users.noreply.github.com>
2021-08-19 10:31:58 +0200

    Nice page about iqos
    Include css/js/saas and of course html ;)

---
+++ strona o iqos/js/slicksettings.js
@@ -0,0 +1,25 @@
+$('.team-carousel').slick(
+
+    {
+        arrows: false,
+        autoplay: true,
+        mobileFirst: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3                    
+                }
+            }
+        ]
+    }
+
+
+);
